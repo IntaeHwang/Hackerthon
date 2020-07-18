4d2782d485394cc227709c63f5bd114feb1085ef
cheonga-code <chengalongo@gmail.com>
2020-07-18 21:52:59 +0900

    07182152 버터나이프 기반 마련

--- app/src/main/java/com/example/hackerthon/MainActivity.java
+++ app/src/main/java/com/example/hackerthon/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.hackerthon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
     String adminId = "김청아";
     int adminAge = 27;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button)
+    Button button1;
+    @BindView(R.id.button3)
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        makeLog(new Object() {}.getClass().getEnclosingMethod().getName()+"()", "관리자 이름 : "+adminId);
-        makeLog(new Object() {}.getClass().getEnclosingMethod().getName()+"()", "관리자 나이 : "+adminAge);
+        makeLog(new Object() {
+        }.getClass().getEnclosingMethod().getName() + "()", "관리자 이름 : " + adminId);
+        makeLog(new Object() {
+        }.getClass().getEnclosingMethod().getName() + "()", "관리자 나이 : " + adminAge);
 
         makeToast("로그인되었습니다.", SHORT_TOAST);
         makeToast("로그인되었습니다.", LONG_TOAST);
 
+    }
+
+    @OnClick(R.id.button2)
+    public void onButton2Clicked() {
+    }
+
+    @OnClick(R.id.button)
+    public void onButtonClicked() {
+    }
+
+    @OnClick(R.id.button3)
+    public void onButton3Clicked() {
     }
 }

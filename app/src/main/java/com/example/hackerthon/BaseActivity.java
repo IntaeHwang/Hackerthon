e668d51d815c1af799bf59e2fd232622e73f90b3
cheonga-code <chengalongo@gmail.com>
2020-07-18 21:42:28 +0900

    07182142 기반 마련

--- app/src/main/java/com/example/hackerthon/BaseActivity.java
+++ app/src/main/java/com/example/hackerthon/BaseActivity.java
@@ -3,22 +3,82 @@ package com.example.hackerthon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
 
     ApplicationClass applicationClass;
+
+    public static final int SHORT_TOAST = 0;
+    public static final int LONG_TOAST = 1;
+
     private static final String TAG = "DeveloperLog";
-    String classname = getClass().getSimpleName().trim();
+    String className = getClass().getSimpleName().trim();
+//    String methodName = new Object() {}.getClass().getEnclosingMethod().getName()+"()";
+
+    //로그 : 액티비티명 + 함수명 + 원하는 데이터를 한번에 보기위한 로그
+    public void makeLog(String methodData, String strData){
+        Log.d(TAG, className+"_"+methodData+" : "+strData);
+
+    }
+
+    //토스트메세지 : 귀찮음을 없애기 위해 토스트를 띄우는 함수를 만듦
+    public void makeToast(String str, int length){
+        if(length == SHORT_TOAST) {
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+        }
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        Log.d(TAG, className+"_onCreate()");
 
         applicationClass = (ApplicationClass)getApplicationContext();
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, className+"_onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, className+"-onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, className+"_onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, className+"_onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, className+"_onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, className+"_onDestroy()");
+        super.onDestroy();
     }
 
 }
--- app/src/main/java/com/example/hackerthon/MainActivity.java
+++ app/src/main/java/com/example/hackerthon/MainActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
 
+    String adminId = "김청아";
+    int adminAge = 27;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeLog(new Object() {}.getClass().getEnclosingMethod().getName()+"()", "관리자 이름 : "+adminId);
+        makeLog(new Object() {}.getClass().getEnclosingMethod().getName()+"()", "관리자 나이 : "+adminAge);
+
+        makeToast("로그인되었습니다.", SHORT_TOAST);
+        makeToast("로그인되었습니다.", LONG_TOAST);
+
     }
 }

fc48e4817c5fdebfb12fe5d69b10d68c341f6bb2
username <webserver3315@naver.com>
2020-07-19 20:29:15 +0900

    Merge branch 'master' of C:\Users\ehdwn\AndroidStudioProjects\Hackerthon with conflicts.

---
+++ app/src/main/java/com/example/hackerthon/TapTapActivity.java
@@ -0,0 +1,14 @@
+package com.example.hackerthon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TapTapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap_tap);
+    }
+}

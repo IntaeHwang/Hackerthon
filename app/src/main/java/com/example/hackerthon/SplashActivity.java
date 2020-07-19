645c6a973110cdac71a702e363575a41fa039856
goeunchan <goyouchan@naver.com>
2020-07-19 16:00:05 +0900

    07191559 SplashActivity 추가

--- app/src/main/java/com/example/hackerthon/SplashActivity.java
+++ app/src/main/java/com/example/hackerthon/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.hackerthon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //스플래쉬 액티비티 2초지속 후 로그인 액티비티로 이동
+        startLoading();
+
+    }
+
+    //2초 후 로그인액티비티로 넘어가는 메소드
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+//                /*자동로그인*/
+//                if (shared_Email!=null) {
+//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(intent);
+//                }else{
+//                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+//                    startActivity(intent);
+//                }
+//
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+
     }
 }

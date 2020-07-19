eab2ac588fc4ed678c41af4a58c4609a63e25bec
cheonga-code <chengalongo@gmail.com>
2020-07-19 16:44:56 +0900

    07191644 build.gradle QR코드 관련 Zxing 라이브러리 추가

--- app/src/main/java/com/example/hackerthon/RoomCreateActivity.java
+++ app/src/main/java/com/example/hackerthon/RoomCreateActivity.java
@@ -1,14 +1,42 @@
 package com.example.hackerthon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RoomCreateActivity extends AppCompatActivity {
+
+    @BindView(R.id.textview_roomCreateActivity_roomMaster)
+    TextView textviewRoomCreateActivityRoomMaster;
+    @BindView(R.id.button_roomCreateActivity_roomOut)
+    Button buttonRoomCreateActivityRoomOut;
+    @BindView(R.id.textview_roomCreateActivity_roomInfo)
+    TextView textviewRoomCreateActivityRoomInfo;
+    @BindView(R.id.imageview_roomCreateActivity_qrCode)
+    ImageView imageviewRoomCreateActivityQrCode;
+    @BindView(R.id.textview_roomCreateActivity_roomStart)
+    TextView textviewRoomCreateActivityRoomStart;
+    @BindView(R.id.linearlayout_roomCreateActivity_roomInfo)
+    LinearLayout linearlayoutRoomCreateActivityRoomInfo;
+    @BindView(R.id.textview_roomCreateActivity_playerInfo)
+    TextView textviewRoomCreateActivityPlayerInfo;
+    @BindView(R.id.recyclerview_roomCreateActivity_playerList)
+    RecyclerView recyclerviewRoomCreateActivityPlayerList;
+    @BindView(R.id.linearlayout_roomCreateActivity_playerList)
+    LinearLayout linearlayoutRoomCreateActivityPlayerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_create);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/example/hackerthon/RoomSearchActivity.java
+++ app/src/main/java/com/example/hackerthon/RoomSearchActivity.java
@@ -1,14 +1,27 @@
 package com.example.hackerthon;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RoomSearchActivity extends AppCompatActivity {
+
+    String roomId; //생성된 방 아이디 = 현재시간을 초 단위까지 받아온 데이터 (중복되지 않기위해)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_search);
+
+//        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        IntentResult
     }
 }

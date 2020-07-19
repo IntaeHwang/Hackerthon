658d2402069a844f4efd392599a44801058afed6
bk4274 <sthe1357@gmail.com>
2020-07-19 15:54:50 +0900

    0719.1547 메인 액티비티 화면

--- app/src/main/java/com/example/hackerthon/MainActivity.java
+++ app/src/main/java/com/example/hackerthon/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.hackerthon;
 import android.os.Bundle;
 import android.widget.Button;
 
-import java.util.HashMap;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -13,13 +11,6 @@ public class MainActivity extends BaseActivity {
 
     String adminId = "김청아";
     int adminAge = 27;
-    String userName = "고은찬";
-    @BindView(R.id.button2)
-    Button button2;
-    @BindView(R.id.button)
-    Button button1;
-    @BindView(R.id.button3)
-    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +23,11 @@ public class MainActivity extends BaseActivity {
         makeLog(new Object() {
         }.getClass().getEnclosingMethod().getName() + "()", "관리자 나이 : " + adminAge);
 
-        makeLog(new Object() {}.getClass().getEnclosingMethod().getName()+"()", "userName : "+userName);
 
-        makeLog(new Object() {
-        }.getClass().getEnclosingMethod().getName() + "()", "userName : " + userName);
-
-
-        makeLog(new Object() {
-        }.getClass().getEnclosingMethod().getName() + "()", "");
 
         makeToast("로그인되었습니다.", SHORT_TOAST);
         makeToast("로그인되었습니다.", LONG_TOAST);
 
-        User user = new User("intae", "qwer1234");
-        HashMap<String, Object> userMap = user.toUserMap();
-
-        applicationClass.databaseReference.child("test").setValue(userMap);
-
     }
 
-    @OnClick(R.id.button2)
-    public void onButton2Clicked() {
-    }
-
-    @OnClick(R.id.button)
-    public void onButtonClicked() {
-    }
-
-    @OnClick(R.id.button3)
-    public void onButton3Clicked() {
-    }
 }

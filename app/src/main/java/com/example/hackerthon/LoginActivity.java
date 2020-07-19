7bb57f40d485d1d8aa3be83b1eb647208565a7a1
goeunchan <goyouchan@naver.com>
2020-07-19 20:53:47 +0900

    07192053 로그인시 셰어드에 유저키값 저장 , 메인화면 넘어감

--- app/src/main/java/com/example/hackerthon/LoginActivity.java
+++ app/src/main/java/com/example/hackerthon/LoginActivity.java
@@ -2,12 +2,19 @@ package com.example.hackerthon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,15 +24,22 @@ import butterknife.OnClick;
 public class LoginActivity extends BaseActivity {
 
     @BindView(R.id.textview_loginActivity_AppName)
-    TextView textviewLoginActivityAppName;
+    TextView textviewLoginActivityAppName; // 상위 앱 이름
     @BindView(R.id.edittext_login_InputEmail)
-    TextInputEditText edittextLoginInputEmail;
+    TextInputEditText edittextLoginInputEmail; // 이메일 입력칸
     @BindView(R.id.edittext_loginActivity_InputPassword)
-    TextInputEditText edittextLoginActivityInputPassword;
+    TextInputEditText edittextLoginActivityInputPassword; //비밀번호 입력칸
     @BindView(R.id.button_loginActivity_login)
-    Button buttonLoginActivityLogin;
+    Button buttonLoginActivityLogin; // 로그인 버트
     @BindView(R.id.button_loginActivity_signUp)
-    Button buttonLoginActivitySignUp;
+    Button buttonLoginActivitySignUp; // 회원가입 버트
+
+
+    //---------------------내가 추가한 변수----------------------
+    String inputEmail;
+    String inputPassword;
+    String userKey;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +56,51 @@ public class LoginActivity extends BaseActivity {
     //로그인 버튼 클릭시
     @OnClick(R.id.button_loginActivity_login)
     public void onButtonLoginActivityLoginClicked() {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+        inputEmail = edittextLoginInputEmail.getText().toString(); //입력 이메일값
+        inputPassword = edittextLoginActivityInputPassword.getText().toString(); //입력 비밀번호
+        userKey = applicationClass.EncodeString(inputEmail); // 키값으로 바꾸기 위해 이메일 . 을 , 으로 바꾼값
+
+        //파이어베이스 로그인
+        applicationClass.firebaseAuth.signInWithEmailAndPassword(inputEmail, inputPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //로그인 성공시
+//                            FirebaseUser user = mAuth.getCurrentUser();
+                            makeLog(new Object() {
+                            }.getClass().getEnclosingMethod().getName() + "()", "로그인 성공 : " );
+                            makeToast("로그인 성공",SHORT_TOAST);
+
+                            //유저키를 Sharedpreference에 저장
+                            applicationClass.mySharedPref.saveStringPref(userKey,userKey);
+
+                            //로그 & 토스트
+                            makeLog(new Object() {
+                            }.getClass().getEnclosingMethod().getName() + "()", "로그인 성공 : " );
+                            makeToast("로그인 성공",SHORT_TOAST);
+
+                            //메인액티비티로 이동
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            
+                        } else {
+                            //로그인 실패시
+                            makeLog(new Object() {
+                            }.getClass().getEnclosingMethod().getName() + "()", "로그인 실패 : " );
+                            makeToast("로그인 실패",SHORT_TOAST);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+
+
+
     }
 
 

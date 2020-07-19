7484d91d12943e669254301aacd7b8e3db015c18
IntaeHwang <intae4708@naver.com>
2020-07-19 17:34:51 +0900

    07191734 Game Class 생성 activity_game_list.xml item_recyclerview_gamelist.xml 완성

---
+++ app/src/main/java/com/example/hackerthon/Game.java
@@ -0,0 +1,49 @@
+package com.example.hackerthon;
+
+import java.util.HashMap;
+
+public class Game {
+    String gamePicture;
+    String gameName;
+    String gameInfo;
+
+    public Game(String gamePicture, String gameName, String gameInfo) {
+        this.gamePicture = gamePicture;
+        this.gameName = gameName;
+        this.gameInfo = gameInfo;
+    }
+
+    public String getGamePicture() {
+        return gamePicture;
+    }
+
+    public void setGamePicture(String gamePicture) {
+        this.gamePicture = gamePicture;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getGameInfo() {
+        return gameInfo;
+    }
+
+    public void setGameInfo(String gameInfo) {
+        this.gameInfo = gameInfo;
+    }
+
+    HashMap<String, Object> toGameMap(){
+        HashMap<String, Object> gameMap = new HashMap<>();
+
+        gameMap.put("gamePicture", gamePicture);
+        gameMap.put("gameName", gameName);
+        gameMap.put("gameInfo", gameInfo);
+
+        return gameMap;
+    }
+}
--- app/src/main/java/com/example/hackerthon/GameListActivity.java
+++ app/src/main/java/com/example/hackerthon/GameListActivity.java
@@ -1,14 +1,45 @@
 package com.example.hackerthon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameListActivity extends AppCompatActivity {
+
+    @BindView(R.id.TextView_GameListActivity_masterText)
+    TextView TextViewGameListActivityMasterText;
+    @BindView(R.id.TextView_GameListActivity_masterName)
+    TextView TextViewGameListActivityMasterName;
+    @BindView(R.id.Button_GameListActivity_exit)
+    Button ButtonGameListActivityExit;
+    @BindView(R.id.RecyclerView_GameListActivity_gameList)
+    RecyclerView RecyclerViewGameListActivityGameList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+        ButterKnife.bind(this);
+
+        
     }
+
+    @OnClick(R.id.Button_GameListActivity_exit)
+    public void onButtonGameListActivityExitClicked() {
+
+    }
+
+    @OnClick(R.id.RecyclerView_GameListActivity_gameList)
+    public void onRecyclerViewGameListActivityGameListClicked() {
+
+    }
+
+
 }

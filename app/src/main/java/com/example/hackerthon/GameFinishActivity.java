eeee8df460fa277d85be56fb272d6621e9ca1228
IntaeHwang <intae4708@naver.com>
2020-07-20 16:50:18 +0900

    07201648 GameFinishActivity 테스트 준비 완료

--- app/src/main/java/com/example/hackerthon/GameFinishActivity.java
+++ app/src/main/java/com/example/hackerthon/GameFinishActivity.java
@@ -1,16 +1,25 @@
 package com.example.hackerthon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class GameFinishActivity extends BaseActivity {
+    @BindView(R.id.recyclerView_GameFinishActivity_gameResult)
+    RecyclerView recyclerViewGameFinishActivityGameResult;
 
     //게임 결과 화면을 종료하고 GameReady Activity로 넘어가기 위한 확인 버튼
     @BindView(R.id.button_GameFinishActivity_dismiss)
@@ -22,13 +31,38 @@ public class GameFinishActivity extends BaseActivity {
         setContentView(R.layout.activity_game_finish);
         ButterKnife.bind(this);
 
+        applicationClass.databaseReference.child("PLAYERLIST").child(getIntent().getStringExtra("RoomNumberKey")).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
 
-
     @OnClick(R.id.button_GameFinishActivity_dismiss)
     public void onViewClicked() {
-
+        //현재 유저가 방장과 같으면 확인
     }
 }
